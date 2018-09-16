6afec05e105b4377a5c1c20f73d2bed967145ead
MinHyeong Kim <romz1212@gmail.com>
2018-09-16 23:40:46 +0900

    fix: cancel command when stream unsubscribed

--- src/spawn.ts
+++ src/spawn.ts
@@ -1,34 +1,53 @@
-import { spawn as nodeSpawn, SpawnOptions } from 'child_process';
-import { Observable, Subject, Subscriber } from 'rxjs';
+import {
+  spawn as nodeSpawn,
+  SpawnOptions as NodeSpawnOptions,
+} from 'child_process';
+import {Observable, Subscriber} from 'rxjs';
 
-export function spawn(command: string, args?: ReadonlyArray<string>, options?: SpawnOptions) {
-  return function spawnImplementation(source: Observable<void>): Observable<Buffer[]> {
+export interface SpawnOptions extends NodeSpawnOptions {
+  threshold?: number;
+}
+
+export function spawn(
+  command: string,
+  args?: ReadonlyArray<string>,
+  options?: SpawnOptions
+) {
+  return function spawnImplementation(
+    source: Observable<void>
+  ): Observable<Buffer[]> {
     return Observable.create((subscriber: Subscriber<Buffer>) => {
-      const subscription = source.subscribe(() => {
-        const buffers: Buffer[] = [];
-        const concatData = (chunk: Buffer) => buffers.push(chunk);
-        const proc = nodeSpawn(command, args, options);
+      const buffers: Buffer[] = [];
+      const proc = nodeSpawn(command, args, options);
+      const subscription = source.subscribe(
+        () => {
+          proc.stdout.on('data', (chunk: Buffer) => buffers.push(chunk));
 
-        proc.stdout.on('data', concatData);
+          proc.stderr.on('data', (chunk: Buffer) => buffers.push(chunk));
 
-        proc.stderr.on('data', concatData);
+          proc.on('error', () => subscriber.error(Buffer.concat(buffers)));
 
-        proc.on('error', err => subscriber.error(Buffer.concat(buffers)));
+          proc.on('close', (code: number) => {
+            proc.removeAllListeners();
+            proc.stdout.removeAllListeners();
+            proc.stderr.removeAllListeners();
 
-        proc.on('close', (code: number) => {
-          if (code > 0) {
-            subscriber.error(Buffer.concat(buffers));
-          } else {
-            subscriber.next(Buffer.concat(buffers));
-            subscriber.complete();
-          }
+            if (code > 0) {
+              subscriber.error(Buffer.concat(buffers));
+            } else {
+              subscriber.next(Buffer.concat(buffers));
+              subscriber.complete();
+            }
+          });
+        },
+        err => subscriber.error(err)
+      );
 
-          proc.removeAllListeners();
-          proc.stdout.removeAllListeners();
-          proc.stderr.removeAllListeners();
-        });
-
-      }, err => subscriber.error(err));
+      subscriber.add(() => {
+        if (!proc.killed) {
+          proc.kill('SIGUSR1');
+        }
+      });
 
       return subscription;
     });
--- test/spawn.spec.ts
+++ test/spawn.spec.ts
@@ -1,14 +1,17 @@
 import {expect} from 'chai';
 import {sync as mkdirp} from 'mkdirp';
 import {sync as rimraf} from 'rimraf';
-import {catchError, map} from 'rxjs/operators';
+import {tap} from 'rxjs/operators';
 
-import {existsSync} from 'fs';
-import {of, throwError} from 'rxjs';
+import {existsSync, writeFileSync} from 'fs';
+import {of} from 'rxjs';
 import {spawn} from '../src/spawn';
 
 describe('spawn', () => {
-  before(() => mkdirp('./tmp'));
+  before(() => {
+    rimraf('./tmp');
+    mkdirp('./tmp');
+  });
 
   it('should execute shell command', done => {
     of(void 0)
@@ -37,6 +40,71 @@ describe('spawn', () => {
         expect(String(err)).to.match(/exists/);
         done();
       });
+  });
+
+  it("should throw previous operator's error", done => {
+    writeFileSync(
+      './tmp/writeFile.sh',
+      '#!/usr/bin/env bash\nsleep 0.1\ntouch test3.txt',
+      {mode: 0x755}
+    );
+
+    of(void 0)
+      .pipe(
+        tap(() => {
+          throw new Error('Hello');
+        }),
+        spawn('bash', ['./tmp/writeFile.sh'], {cwd: './tmp'})
+      )
+      .subscribe(void 0, err => {
+        expect(String(err)).to.equal('Error: Hello');
+        expect(existsSync('./tmp/test3.txt')).to.be.false;
+        done();
+      });
+  });
+
+  it('should cancel command when unsubscribe', done => {
+    writeFileSync(
+      './tmp/cancel.sh',
+      '#!/usr/bin/env bash\nsleep 0.1\necho Hello',
+      {mode: 0x755}
+    );
+
+    let output = '';
+
+    const sub = of(void 0)
+      .pipe(spawn('bash', ['./tmp/cancel.sh']))
+      .subscribe(o => (output = String(o)));
+
+    sub.add(() => {
+      expect(output).to.equal('');
+      done();
+    });
+
+    Promise.resolve().then(() => sub.unsubscribe());
+  });
+
+  it('should cancel command when unsubscribe2', done => {
+    let exist = false;
+
+    const sub = of(void 0)
+      .pipe(
+        spawn('git', ['clone', 'github.com/johnny-mh/rxjs-shell-operators'], {
+          cwd: './tmp',
+        })
+      )
+      .subscribe({
+        next() {
+          exist = existsSync('./tmp/rxjs-shell-operators');
+        },
+      });
+
+    sub.add(() => {
+      expect(exist).to.be.false;
+      done();
+    });
+
+    Promise.resolve().then(() => sub.unsubscribe());
   });
 
   after(() => rimraf('./tmp'));
