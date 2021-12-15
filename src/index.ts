df844956f5f1541713641eb61944a17beaad59a5
johnny-mh <romz1212@gmail.com>
2021-12-16 02:30:05 +0900

    fix: export newly added operator

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,11 @@ export * from './execFile';
 export * from './fork';
 export * from './spawn';
 export * from './models';
-export {trim, throwIf, throwIfStdout, throwIfStderr} from './operators';
+export {
+  trim,
+  throwIf,
+  throwIfStdout,
+  throwIfStderr,
+  execWithStdin,
+} from './operators';
 export {spawnEnd, ShellError, listenTerminating} from './util';
